82d5beb6a07a8b21d5b8ba42ace9a87032eeb5a9
Adrian <adrianjayson13@gmail.com>
2017-10-14 20:51:22 +0800

    Adds Hourly Forecast feature, when learning about RecyclerViews

---
+++ app/src/main/java/adrianjayson/com/stormy/adapters/HourAdapter.java
@@ -0,0 +1,68 @@
+package adrianjayson.com.stormy.adapters;
+
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import adrianjayson.com.stormy.R;
+import adrianjayson.com.stormy.weather.Hour;
+
+/**
+ * Created by adrian on 14/10/2017.
+ */
+
+public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
+
+    private Hour[] mHours;
+
+    public HourAdapter(Hour[] hours) {
+        mHours = hours;
+    }
+
+    @Override
+    public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.hourly_list_item, parent, false);
+        HourViewHolder viewHolder = new HourViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(HourViewHolder holder, int position) {
+        holder.bindHour(mHours[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHours.length;
+    }
+
+    public class HourViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTimeLabel;
+        public TextView mSummaryLabel;
+        public TextView mTemperatureLabel;
+        public ImageView mIconLabel;
+
+        public HourViewHolder(View itemView) {
+            super(itemView);
+
+            mTimeLabel = itemView.findViewById(R.id.timeLabel);
+            mSummaryLabel = itemView.findViewById(R.id.summaryLabel);
+            mTemperatureLabel = itemView.findViewById(R.id.temperatureLabel);
+            mIconLabel = itemView.findViewById(R.id.iconImageView);
+        }
+
+        public void bindHour(Hour hour) {
+            mTimeLabel.setText(hour.getHour());
+            mSummaryLabel.setText(hour.getSummary());
+            mTemperatureLabel.setText(hour.getTemperature() + "");
+            mIconLabel.setImageResource(hour.getIconId());
+        }
+    }
+}
--- app/src/main/java/adrianjayson/com/stormy/ui/MainActivity.java
+++ app/src/main/java/adrianjayson/com/stormy/ui/MainActivity.java
@@ -39,6 +39,7 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String DAILY_FORECAST = "DAILY_FORECAST";
+    public static final String HOURLY_FORECAST = "HOURLY_FORECAST";
 
     private Forecast mForecast;
 
@@ -266,6 +267,13 @@ public class MainActivity extends AppCompatActivity {
     public void startDailyActivity(View view) {
         Intent intent = new Intent(this, DailyForecastActivity.class);
         intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
+        startActivity(intent);
+    }
+
+    @OnClick(R2.id.hourlyButton)
+    public void startHourlyActivity(View view) {
+        Intent intent = new Intent(this, HourlyForecastActivity.class);
+        intent.putExtra(HOURLY_FORECAST, mForecast.getHourlyForecast());
         startActivity(intent);
     }
 }
